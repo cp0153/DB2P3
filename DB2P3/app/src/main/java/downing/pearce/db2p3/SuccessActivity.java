bc99201a905350c97890d55b40211f7491cf6421
Jason Downing <jdown1994@gmail.com>
2017-04-13 15:10:38 -0400

    this commit is working somewhat. I need to connect it to our real local host and try sending or getting values now

---
+++ DB2P3/app/src/main/java/downing/pearce/db2p3/SuccessActivity.java
@@ -0,0 +1,17 @@
+package downing.pearce.db2p3;
+
+/**
+ * Created by asus on 4/13/17.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
